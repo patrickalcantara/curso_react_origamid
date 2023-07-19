be86ac52a464a74f97e12db12023172285c6fab4
patrickdimas <patrickalcantara214@gmail.com>
2023-07-19 17:54:27 -0300

    fix atividade 0401

--- aula_0401/src/App.jsx
+++ aula_0401/src/App.jsx
@@ -78,87 +78,23 @@ const App = () => {
 
   return (
     <>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="nome">Nome</label>
-        <input
-          type="text"
-          name="nome"
-          id="nome"
-          value={form.nome}
-          onChange={handleChange}
-        />
+      <form onSubmit={handleSubmit} id="form">
+        {formFields.map(({ id, label, type }) => {
+          return (
+            <div key={id}>
+              <label htmlFor={id}>{label}</label>
+              <input
+                type={type}
+                name={id}
+                id={id}
+                value={form[id]}
+                onChange={handleChange}
+                autoComplete="off"
+              />
+            </div>
+          );
+        })}
 
-        <label htmlFor="email">Email</label>
-        <input
-          type="email"
-          name="email"
-          id="email"
-          value={form.email}
-          onChange={handleChange}
-        />
-
-        <label htmlFor="senha">Senha</label>
-        <input
-          type="password"
-          name="senha"
-          id="senha"
-          value={form.senha}
-          onChange={handleChange}
-        />
-
-        <label htmlFor="cep">CEP</label>
-        <input
-          type="text"
-          name="cep"
-          id="cep"
-          value={form.cep}
-          onChange={handleChange}
-        />
-
-        <label htmlFor="rua">Rua</label>
-        <input
-          type="text"
-          name="rua"
-          id="rua"
-          value={form.rua}
-          onChange={handleChange}
-        />
-
-        <label htmlFor="numero">Numero</label>
-        <input
-          type="text"
-          name="numero"
-          id="numero"
-          value={form.numero}
-          onChange={handleChange}
-        />
-
-        <label htmlFor="bairro">Bairro</label>
-        <input
-          type="text"
-          name="bairro"
-          id="bairro"
-          value={form.bairro}
-          onChange={handleChange}
-        />
-
-        <label htmlFor="cidade">Cidade</label>
-        <input
-          type="text"
-          name="cidade"
-          id="cidade"
-          value={form.cidade}
-          onChange={handleChange}
-        />
-
-        <label htmlFor="estado">Estado</label>
-        <input
-          type="text"
-          name="estado"
-          id="estado"
-          value={form.estado}
-          onChange={handleChange}
-        />
         <button>Enviar</button>
         {response && response.ok && <p>Usuário cadastrado com sucesso!</p>}
       </form>
